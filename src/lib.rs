1629212f4aff471128f11537441de23fef40fab6
darklamp <alessandro17.villa@mail.polimi.it>
2021-01-12 18:50:22 +0100

    improved

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ use std::cmp::Ordering;
 use rayon::prelude::*;
 use wasm_bindgen::prelude::*;
 extern crate serde_json;
+extern crate console_error_panic_hook;
+use std::panic;
 
 
 #[macro_use]
@@ -28,55 +30,32 @@ impl PartialEq for Row {
     }
 }
 
-#[derive(Serialize, Deserialize, Eq)]
-pub struct RowDate {
-    datetime: String,
-    questionnaireId: i32,
-    name: String, 
-    image: String,
-}
 
-impl Ord for RowDate {
-    fn cmp(&self, other: &Self) -> Ordering {
-        if js_sys::Date::new(&JsValue::from_serde(&self.datetime).unwrap()).get_time() - js_sys::Date::new(&JsValue::from_serde(&other.datetime).unwrap()).get_time()  > 0.0 {
+#[wasm_bindgen]
+pub fn sort(data: JsValue, reverse: bool, by_date: bool) -> JsValue {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    
+    let order_by_date = |a : &Row, b : &Row| {
+        if js_sys::Date::new(&JsValue::from_serde(&a.datetime).unwrap()).get_time() - js_sys::Date::new(&JsValue::from_serde(&b.datetime).unwrap()).get_time()  > 0.0 {
             Ordering::Greater
         }
         else {
             Ordering::Less
         }
-    }
-}
+    };
+    let mut parsed_data: Vec<Row> = data.into_serde().unwrap();
 
-impl PartialOrd for RowDate {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for RowDate {
-    fn eq(&self, other: &Self) -> bool {
-        self.datetime == other.datetime
-    }
-}
-
-#[wasm_bindgen]
-pub fn sort(data: JsValue, reverse: bool, by_date: bool) -> JsValue {
     match by_date {
         true => {
-            let mut parsed_data: Vec<RowDate> = data.into_serde().unwrap();
-            parsed_data.par_sort_unstable();
-            if reverse {
-                parsed_data.reverse();
-            }
-            return JsValue::from_serde(&parsed_data).unwrap();
+            parsed_data.par_sort_unstable_by(order_by_date);
+            
         }
         _ => {
-            let mut parsed_data: Vec<Row> = data.into_serde().unwrap();
             parsed_data.par_sort_unstable();
-            if reverse {
-                parsed_data.reverse();
-            }
-            return JsValue::from_serde(&parsed_data).unwrap();
         }
     }
+    if reverse {
+        parsed_data.reverse();
+    }
+    JsValue::from_serde(&parsed_data).unwrap()
 }
